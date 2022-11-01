ab8b9b406397489857c217a621d8a62682d7152a
Karan Vaswani <53395550+karan-17@users.noreply.github.com>
2022-11-02 02:28:06 +0530

    appwrite 1.0 support

---
+++ flutter_appwrite/lib/res/constant.dart
@@ -0,0 +1,7 @@
+class AppConstant {
+static const String projectid = '6358f077598701771cb7';
+static const String endPoint = 'http://192.168.1.12/v1';
+static const String database = '6358f3893420e9ee5b8d';
+static const String collectionId = '6358f3ab531448a7be76';
+static const bucketId = '63617da0e1e0b810ca92';
+}
